bdde58ec2e5d9883886b00112d445a71298c81e4
JohnMacJr <111828057+JohnMacJr@users.noreply.github.com>
2024-09-28 15:35:39 -0700

    Added first shape
    spent too long adding first shape to canvas.

--- Assignment-2/shapes.js
+++ Assignment-2/shapes.js
@@ -1,17 +1,37 @@
-
 let gl = undefined;
+let angle = 0.0;
+let ms;
+let cone;
 
 function init() {
     let canvas = document.getElementById("webgl-canvas");
     gl = canvas.getContext("webgl2");
     if (!gl) { alert("Your Web browser doesn't support WebGL 2\nPlease contact Dave"); }
 
-    // Add initialization code here
+    gl.clearColor(0.4, 0.5, 0.8, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    cone = new Cone(gl, 36);
+    ms = new MatrixStack();
+
+    render();
 }
 
 function render() {
-    // Add rendering code here
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    angle += 1.0;
+    angle %= 360.0;
+
+    ms.push();
+    ms.translate([0.5, 0.5, 0.0]);
+    ms.rotate(angle, [0, 1, 1]);
+    ms.scale(0.1);
+    cone.MV = ms.current();  
+    cone.draw();             
+    ms.pop();
+    
+    requestAnimationFrame(render);
 }
 
 window.onload = init;
-
