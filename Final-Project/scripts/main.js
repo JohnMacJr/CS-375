21a116d58bcf0432eee6cf072bac3be88097b5e9
JohnMacJr <111828057+JohnMacJr@users.noreply.github.com>
2024-12-04 01:08:46 -0800

    Added model and texture scrolling
    added model of a ship, added texture scrolling to the plane to simulate flight, and added a "temp" light source.

--- Final-Project/scripts/main.js
+++ Final-Project/scripts/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 
@@ -10,13 +11,26 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a green cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-cube.position.x = +2;
-cube.position.y = +2;
+// Lighting temporary before
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(10, 10, 10);
+scene.add(light);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+// Load 3D Model
+const loader = new GLTFLoader();
+loader.load(
+    'assets/Spaceship2.glb',
+    (gltf) => {
+        const model = gltf.scene;
+        model.position.set(3, 0, 0);
+        model.scale.set(1, 1, 1);
+        model.rotation.y = -Math.PI / 2;
+        scene.add(model);
+    }
+);
 
 // Textured Plane
 const textureLoader = new THREE.TextureLoader();
@@ -25,17 +39,21 @@ groundTexture.wrapS = THREE.RepeatWrapping;
 groundTexture.wrapT = THREE.RepeatWrapping;
 groundTexture.repeat.set(10, 10);
 
-const planeGeometry = new THREE.PlaneGeometry(100, 100);
+const planeGeometry = new THREE.PlaneGeometry(120, 170);
 const planeMaterial = new THREE.MeshBasicMaterial({ map: groundTexture });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -Math.PI / 2;
 plane.position.y = -0.5;
 scene.add(plane);
 
-camera.position.set(8, 8, 0); 
-camera.lookAt(0, 0, 0);      
+camera.position.set(8, 8, 0);
+camera.lookAt(0, 0, 0);
+
+const scrollSpeed = 0.05;
 
 function animate() {
+    groundTexture.offset.x -= scrollSpeed;
+
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
